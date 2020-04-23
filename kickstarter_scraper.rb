8285f394f1a2534074098524597f00a48096fdec
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-23 04:28:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require 'nokogiri'
 require 'pry'
 
@@ -7,10 +8,15 @@ require 'pry'
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+=======
+require "nokogiri"
+require "pry"
+>>>>>>> f9928926e39b1c0bf06ae189537685dfc12bcf95
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+<<<<<<< HEAD
 
   projects = {}
 
@@ -27,3 +33,14 @@ def create_project_hash
   # return the projects hash
   projects
 end
+=======
+  binding.pry
+end
+
+create_project_hash
+# projects: kickstarter.css("li.project.grid_4").first
+# title: project.css("h2.bbcard_name strong a").text
+# image: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("h2.bbcard_name strong a href")
+>>>>>>> f9928926e39b1c0bf06ae189537685dfc12bcf95
